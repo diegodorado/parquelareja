349f85c4ada2ec8f29e86c62f845dafac13c88ee
Diego Dorado <diegodorado@gmail.com>
2017-04-18 12:33:09 -0300

    modified deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -6,8 +6,8 @@ set :use_sudo, false
 ssh_options[:port] = 9022
 default_run_options[:pty] = true
 
-#set :deploy_via, :copy
-set :deploy_via, :remote_cache
+set :deploy_via, :copy
+#set :deploy_via, :remote_cache
 
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain                         # This may be the same as your `Web` server
