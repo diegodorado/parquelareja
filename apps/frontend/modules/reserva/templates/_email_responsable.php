2629488035e53b28abb389d94aa79931eaf045e9
santiago <santiago@redhumanista.org>
2017-08-29 16:00:16 -0300

    se agregó el subject al mail de reserva multiuso, se quitó el mensaje con el mail de eva de la página de reservas

---
+++ apps/frontend/modules/reserva/templates/_email_responsable.php
@@ -0,0 +1,5 @@
+﻿<div>
+	Por medio del presente le confirmamos que hemos recibido su pedido de reserva.<br>
+	Mantengase en órbita y más temprano que tarde recibirá la confirmación.
+	Besitos, la comisión.
+</div>
